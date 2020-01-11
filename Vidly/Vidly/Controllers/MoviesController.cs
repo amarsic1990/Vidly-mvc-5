68acb881ab4074173716e96007115a71faf779a5
Ante Maršić <amarsic@hattrick.local>
2020-01-11 23:33:20 +0100

    Action Results

--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -10,11 +10,18 @@ namespace Vidly.Controllers
     public class MoviesController : Controller
     {
         // GET: Movies/Random
+        // vracamo instancu od klase koja nasljeduje iz ACTIONRESULT...
         public ActionResult Random()
         {
 
             var movie = new Movie() { Name = "Shrek!" };
-            return View(movie);
+            //return View(movie);
+            //return Content("Hello World!");
+            //return HttpNotFound();
+            //return new EmptyResult();
+
+            // katkad kada redirectamo moramo passati argumente target akciji.... to radimo s anonimus objektom kao 3 argumentom
+            return RedirectToAction("Index", "Home", new { page = 1, sortby = "Name" });
         }
     }
 }
