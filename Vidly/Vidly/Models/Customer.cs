5b72299010b264398dda01c35a8a1a035f003182
Ante Maršić <amarsic@hattrick.local>
2020-01-12 21:11:40 +0100

    Overriding Conventions

--- Vidly/Vidly/Models/Customer.cs
+++ Vidly/Vidly/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +9,11 @@ namespace Vidly.Models
     public class Customer
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
+
         public bool IsSubscribedToNewsletter { get; set; }
 
         // ovo je navigation property jel nam dozvoljava da idemo od jednog type-a do drugog
