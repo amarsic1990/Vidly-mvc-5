ddc0ee40cb2a541a4e9c32c52c8fe02dc4e97736
Ante Maršić <amarsic@hattrick.local>
2020-01-12 23:49:18 +0100

    Querying Objects

--- Vidly/Vidly/Controllers/CustomersController.cs
+++ Vidly/Vidly/Controllers/CustomersController.cs
@@ -9,24 +9,54 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
 
-        private List<Customer> customers = new List<Customer>()
+        // ovo moramo inicijalizirati u konstruktoru
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // DbContext je disposable objekt pa ga moramo dispose-ati to jest osloboditi memoriju...
+        // OSLOBAĐA UNMANAGED RESURSE
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+
+        //private List<Customer> customers = new List<Customer>()
+        //    {
+        //        new Customer {Name = "Ante Maršić", Id = 1},
+        //        new Customer {Name = "Danica Maršić", Id = 2}
+        //    };
+
+
+
+        // GET: Customers
+        // to list I singe or default metode odmag izvrsavaju upit...
+        public ActionResult Index()
+        {
+            var customers = _context.Customers.ToList(); ;
+            return View(customers);
+        }
+
+
+        // to list I singe or default metode odmag izvrsavaju upit...
+        public ActionResult Details(int? id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            return View(customer);
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
             {
                 new Customer {Name = "Ante Maršić", Id = 1},
                 new Customer {Name = "Danica Maršić", Id = 2}
             };
-
-        // GET: Customers
-        public ActionResult Index()
-        {
-            return View(customers);
-        }
-
-        public ActionResult Details(int? id)
-        {
-            var customer = customers.SingleOrDefault(c => c.Id == id);
-
-            return View(customer);
         }
     }
 }
